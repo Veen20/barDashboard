6d1d31074c4c2f4bdf15fd3062a320941ca04a83
Veen20 <150326542+Veen20@users.noreply.github.com>
2025-08-16 17:25:03 +0700

    Update dashboard_interaktif.py

--- dashboard_interaktif.py
+++ dashboard_interaktif.py
@@ -46,15 +46,9 @@ st.set_page_config(
 
 # -----------------------------
 # Load CSS (style.css di root)
-# -----------------------------
-def load_css(file_path=".streamlit/style.css"):
-    if os.path.exists(file_path):
-        with open(file_path) as f:
-            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    else:
-        st.warning(f"⚠️ style.css tidak ditemukan di: {file_path}")
-
-load_css()
+# Load file CSS
+with open("streamlit/style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # -----------------------------
 # Konstanta & Palet
